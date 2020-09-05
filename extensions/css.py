1cc4fdb9c1d58f7e25eba1f9b924ffeb77d63ba7
Vincent Bernat <vincent@bernat.ch>
2020-09-05 08:59:03 +0200

    build: replace cssprefixer by autoprefixer

--- extensions/css.py
+++ extensions/css.py
@@ -3,12 +3,37 @@
 CSS plugins
 """
 
+import subprocess
 from hyde.plugin import Plugin
-import cssprefixer
+
 
 class CSSPrefixerPlugin(Plugin):
     """Run CSS prefixer"""
     def text_resource_complete(self, resource, text):
-        if not resource.source_file.kind in ("less", "css"):
+        if resource.source_file.kind not in ("less", "css"):
             return
-        return cssprefixer.process(text, debug=False, minify=True)
+        p = subprocess.Popen(['nodejs', '-e', """
+var autoprefixer = require('autoprefixer');
+var mqpacker = require('css-mqpacker');
+var cssnano = require('cssnano');
+var postcss = require('postcss');
+var input = '';
+
+process.stdin.setEncoding('utf8')
+process.stdin.on('readable', function() {
+  var chunk = process.stdin.read();
+  if (chunk) {
+    input += chunk;
+  }
+});
+process.stdin.on('end', function() {
+  postcss([autoprefixer, mqpacker, cssnano({reduceIdents: false})])
+        .process(input, { from: undefined })
+        .then(function(result) {
+          process.stdout.write(result.css.toString());
+        });
+});
+        """], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        stdout, _ = p.communicate(text.encode('utf-8'))
+        assert p.returncode == 0
+        return stdout.decode('utf-8')
